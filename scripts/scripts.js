537866537830697b4a0fdca2be3559ea3f4a8a7d
Jeremy Browne <jeremybrowne1991@hotmail.com>
2021-07-18 11:53:22 +1000

    Updated to use Math.round instead of Math.floor

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,8 @@
 function newQuestionData() {
-    document.getElementById("elevation").innerHTML = Math.floor(Math.random() * 8000) + " ft";
-    document.getElementById("QNH").innerHTML = (Math.random() < 0.5 ? 1013 - Math.floor(Math.random() * 30) : 1013 + Math.floor(Math.random() * 30)) + " hPa";
+    document.getElementById("elevation").innerHTML = Math.round(Math.random() * 8000) + " ft";
+    document.getElementById("QNH").innerHTML = (Math.random() < 0.5 ? 1013 - Math.round(Math.random() * 30) : 1013 + Math.round(Math.random() * 30)) + " hPa";
     document.getElementById("answer").innerHTML = "";
-    document.getElementById("actualTemp").innerHTML = (Math.random() < 0.5 ? 15 - Math.floor(Math.random() * 10) : 15 + Math.floor(Math.random() * 10)) + "°c";
+    document.getElementById("actualTemp").innerHTML = (Math.random() < 0.5 ? 15 - Math.round(Math.random() * 10) : 15 + Math.round(Math.random() * 10)) + "°c";
     document.getElementById("ISATemp").innerHTML = "";
     document.getElementById("ISADev").innerHTML = "";
     document.getElementById("densityAltitude").innerHTML = "";
@@ -16,12 +16,13 @@ function getPressureHeight() {
 
 function getDensityHeight() {
     let actualTemp = parseInt(document.getElementById("actualTemp").innerHTML);
-    let ISATemp = 15 - Math.floor((getPressureHeight() / 1000) * 2);
+    let ISATemp = Math.round(15 - Math.round(getPressureHeight()) / 1000 * 2);
     let ISADev = actualTemp - ISATemp;
     let densityAltitude = getPressureHeight() + 120 * ISADev;
     document.getElementById("ISATemp").innerHTML = ISATemp + "°c";
     document.getElementById("ISADev").innerHTML = actualTemp - ISATemp + "°c";
     document.getElementById("densityAltitude").innerHTML = densityAltitude + " ft";
+    return densityAltitude;
 }
 
 function toggleFormulas() {
